95f8ba19cc3591b0b89f328f0f76f648d01c9c9e
Corey Scribner <coreys@onenevada.org>
2018-05-30 09:06:59 -0700

    Added Compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -12,6 +13,7 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -25,4 +27,16 @@ gulp.task('js', function() {
 		.pipe(concat('script.js')) //HTML file calls script.js for concatination work
 		.pipe(browserify()) //after concatination, run script through browserify to let us use node.js modules in the browser
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
+
 });
